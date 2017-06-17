66a304c58bdd322af0e3ffb6200ce8408a2310de
SUZUMIYA\Taejoon <djaxo3902@naver.com>
2017-06-17 17:22:02 +0900

    수정

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var app = require('express').createServer()
 	,io = require('socket.io').listen(app);
 
-// 여기서 포트는 지정할 수 없고 하나만 쓸 수 있음. 미리정해진 상수.
 app.listen(process.env.PORT);
 console.log("port = " + process.env.PORT);
 
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/DNF_Raid.html');
 });
 
 io.sockets.on('connection', function (socket) {
